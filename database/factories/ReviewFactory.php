03a947ff7bcd3df36148d41fea7e94b3c28639ed
Trong Nguyen <trong.nguyen@airinu.com>
2018-10-07 01:31:00 +0700

    Create factory & seed db

--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -5,5 +5,11 @@ use Faker\Generator as Faker;
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
         //
+        'product_id' => function(){
+            return factory(App\Model\Product::class)->create()->id;
+        },
+        'customer' => $faker->name,
+        'review' => $faker->paragraph,
+        'star' => $faker->numberBetween(0,5),
     ];
 });
